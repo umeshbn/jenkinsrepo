6662e587465076449923e27d1112c2e3d29b5af1
umeshinflowsol <umesh.neelaguri@inflowsol.com>
2024-09-08 00:05:57 +0530

    Initial commit

---
+++ src/test/java/com/mypratice/program/ProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypratice.program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
