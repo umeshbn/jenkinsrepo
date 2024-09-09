e5eb95d34ae53aeaf2c5e38bafd2a3164aa153a9
umeshinflowsol <umesh.neelaguri@inflowsol.com>
2024-09-09 10:16:26 +0530

    added dependencies

---
+++ src/main/java/com/mypratice/program/ProgramApplication.java
@@ -0,0 +1,12 @@
+package com.mypratice.program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramApplication {
+
+public static void main(String[] args){
+    SpringApplication.run(ProgramApplication.class,args);
+}
+}
